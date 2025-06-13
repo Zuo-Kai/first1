a79174a36dcdbff42433147a34182df9dca990ab
Zuo-Kai <alexhsiao940201@gmail.com>
2025-06-13 22:57:52 +0800

    Add files via upload

---
+++ 改編版程式碼/StockInput.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#ifndef STOCK_INPUT_H
+#define STOCK_INPUT_H
+
+#include <string>
+
+int 輸入股數(const std::string& 動作);
+double 輸入價格(const std::string& 類型);
+
+#endif
---
+++ 改編版程式碼/StockUtils.h
@@ -0,0 +1,7 @@
+
+#ifndef STOCK_UTILS_H
+#define STOCK_UTILS_H
+
+const int 一張股數 = 1000;
+
+#endif
